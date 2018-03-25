19181d81782f76d48d7ba0703b3566d818b3c125
Sara Elmoghazy <sara.elmoghazy@yahoo.com>
2018-03-25 18:32:50 +0200

    init project

---
+++ app/src/main/java/com/ntg/user/sa2aia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ntg.user.sa2aia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
